68b9e0e9975a28e852036103c3639435a3dad9c6
y25511 <y25511@iwate-it.ac.jp>
2026-02-16 16:09:34 +0900

    main.jsを書きました。
    使用する変数とJSONデータの読み込み、ゲーム開始の関数を書きました。

--- quiz/main.js
+++ quiz/main.js
@@ -1,1 +1,36 @@
-/*担当：　髙橋拓馬*/
+let allquiz = [];  // JSONから読み込んだ全データ
+let quiz = [];   // 出題する10問
+let count = 0;  // 今何問目か
+let score = 0;  // 正解数
+let timer;  // タイマーの入れ物
+let timeLeft = 20  // 残り時間
+
+// Jsonのデータを読み込み
+fetch('quiz.json')
+    .then(response => response.json())
+    .then(data => {
+        allquiz = data;
+        game();  // データの読み込みが終わったらゲーム開始
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('question').textContent = 'データの読み込みに失敗しました。';
+    });
+
+// ゲームの初期化
+function game() {
+    // シャッフルして１０問取得してquizに入れる
+    quiz = allquiz.sort(() => Math.random( ) - 0.5).slice(0, 10);
+    count = 0;
+    score = 0;
+    showquiz();
+}
+
+// 問題表示
+function showquiz() {
+    if(count >= 10) {
+        finishgame();
+        return;
+    }
+    
+}
