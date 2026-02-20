1715cf10b09913a3669c671c5cffd7f4c87be70c
y25506-do <y25506@iwate-it.ac.jp>
2026-02-20 12:00:18 +0900

    Update index.js

--- quiz/index.js
+++ quiz/index.js
@@ -1,2 +1,7 @@
+// 初回アクセスならコインをリセット
+if (!localStorage.getItem("firstvisit")) {
+    localStorage.setItem("totalcoin", 0);
+    localStorage.setItem("firstvisit", "true");
+}
 let totalCoin = Number(localStorage.getItem("totalcoin")) || 0;
 document.getElementById("totalcoin").textContent = "獲得コイン： " + totalCoin;
