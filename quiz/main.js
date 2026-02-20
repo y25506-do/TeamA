6c95690fb8ce81920b729af72ab8037dfbe02866
y25506-do <y25506@iwate-it.ac.jp>
2026-02-20 11:04:20 +0900

    コイン導入について

--- quiz/main.js
+++ quiz/main.js
@@ -139,15 +139,32 @@ function finishgame(){
     window.location.href="result.html";
 }
 //結果画面用
-score = localStorage.getItem("score");
+score = Number(localStorage.getItem("score"));
 
 document.getElementById("comment1").textContent= "あなたのスコアは 10問中" + score + "問正解です。" 
 
+let coin = 0;
 if(score >= 0 && score <= 3)
+{
+    coin = coin + 1;
+    document.getElementById("coin").textContent = "獲得コイン： " + coin;
     document.getElementById("comment2").textContent = "分からない問題多かったでしょう。中には関心を持つ問題や問題を考えた人の特徴が見えたと思います。少しずつスコアをあげましょう。";
+}
 else if(score >= 4 && score <= 6)
+{
+    coin = coin + 2;
+    document.getElementById("coin").textContent = "獲得コイン： " + coin;
     document.getElementById("comment2").textContent = "ある程度問題解けたかな？問題を解いているとその人がどんな気持ちで考えたのか分かっていくはずです。君なら満点目指せるよ。";
+}
 else if(score >= 7 && score <= 9)
+{   
+    coin = coin + 3;
+    document.getElementById("coin").textContent = "獲得コイン： " + coin;
     document.getElementById("comment2").textContent = "ほぼほぼ問題解けるようになったね。この調子で満点を取れたらいいですね。頑張りましょう。";
+}
 else
+{
+    coin = coin + 3;
+    document.getElementById("coin").textContent = "獲得コイン： " + coin;
     document.getElementById("comment2").textContent = "満点取れましたね！しかし、問題は合計４０問あります。全部解けるように今後も頑張りましょう。";
+}
